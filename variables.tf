4fa1cc6457b6721a24f08270fb6f9a076c3b0350
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2023-03-30 00:26:22 -0300

    add type

--- variables.tf
+++ variables.tf
@@ -27,10 +27,12 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidrs" {
+  type = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "public_subnet_availability_zones" {
+  type = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
 }
 
